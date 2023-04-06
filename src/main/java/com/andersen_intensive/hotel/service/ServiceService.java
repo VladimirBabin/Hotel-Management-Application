caef9660060c9556feff1004b80717610a58265c
Giorgi <1giorgiadamia@gmail.com>
2023-04-06 11:03:41 +0400

    add creation of service, sort services by name and price

--- src/main/java/com/andersen_intensive/hotel/service/ServiceService.java
+++ src/main/java/com/andersen_intensive/hotel/service/ServiceService.java
@@ -1,4 +1,11 @@
 package com.andersen_intensive.hotel.service;
 
+import com.andersen_intensive.hotel.models.Service;
+
+import java.util.List;
+
 public interface ServiceService {
+    Service saveService(String name, double price);
+    List<Service> sortByName();
+    List<Service> sortByPrice();
 }
--- src/main/java/com/andersen_intensive/hotel/service/ServiceServiceImpl.java
+++ src/main/java/com/andersen_intensive/hotel/service/ServiceServiceImpl.java
@@ -1,4 +1,36 @@
 package com.andersen_intensive.hotel.service;
 
-public class ServiceServiceImpl {
+import com.andersen_intensive.hotel.models.Service;
+import com.andersen_intensive.hotel.repository.ServiceRepository;
+
+import java.util.Comparator;
+import java.util.List;
+
+public class ServiceServiceImpl implements ServiceService {
+
+    private final ServiceRepository serviceRepository;
+
+    public ServiceServiceImpl(ServiceRepository serviceRepository) {
+        this.serviceRepository = serviceRepository;
+    }
+
+    @Override
+    public Service saveService(String name, double price) {
+        Service service = new Service(name, price);
+        return serviceRepository.addService(service);
+    }
+
+    @Override
+    public List<Service> sortByName() {
+        List<Service> sortedServices = serviceRepository.getAllServices();
+        sortedServices.sort((s1, s2) -> s1.getName().compareToIgnoreCase(s2.getName()));
+        return sortedServices;
+    }
+
+    @Override
+    public List<Service> sortByPrice() {
+        List<Service> sortedServices = serviceRepository.getAllServices();
+        sortedServices.sort(Comparator.comparingDouble(Service::getPrice));
+        return sortedServices;
+    }
 }
