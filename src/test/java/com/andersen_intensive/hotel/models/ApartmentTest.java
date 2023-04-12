600f4b8d92d37489e3c1e1778f48658f73d2b064
Svetlana <mikolapols@gmail.com>
2023-04-12 11:52:16 +0200

    ApartmentTest. Without DTO

--- src/test/java/com/andersen_intensive/hotel/models/ApartmentTest.java
+++ src/test/java/com/andersen_intensive/hotel/models/ApartmentTest.java
@@ -7,52 +7,6 @@ import java.math.BigDecimal;
 import static org.junit.jupiter.api.Assertions.*;
 
 class ApartmentTest {
-
-    @Test
-    void getApartmentId() {
-        Apartment apartment = new Apartment(1, BigDecimal.valueOf(100), ApartmentType.SINGLE);
-        assertEquals(1, apartment.getApartmentId());
-    }
-
-    @Test
-    void getApartmentPrice() {
-        Apartment apartment = new Apartment(1, BigDecimal.valueOf(100), ApartmentType.SINGLE);
-        assertEquals(BigDecimal.valueOf(100), apartment.getApartmentPrice());
-    }
-
-    @Test
-    void setPrice() {
-        Apartment apartment = new Apartment(1, BigDecimal.valueOf(100), ApartmentType.SINGLE);
-        apartment.setPrice(BigDecimal.valueOf(150));
-        assertEquals(BigDecimal.valueOf(150), apartment.getApartmentPrice());
-    }
-
-    @Test
-    void getApartmentType() {
-        Apartment apartment = new Apartment(1, BigDecimal.valueOf(100), ApartmentType.SINGLE);
-        assertEquals(ApartmentType.SINGLE, apartment.getApartmentType());
-    }
-
-    @Test
-    void getApartmentStatus() {
-        Apartment apartment = new Apartment(1, BigDecimal.valueOf(100), ApartmentType.SINGLE);
-        assertEquals(ApartmentStatus.AVAILABLE, apartment.getApartmentStatus());
-    }
-
-    @Test
-    void setApartmentStatus() {
-        Apartment apartment = new Apartment(1, BigDecimal.valueOf(100), ApartmentType.SINGLE);
-        apartment.setApartmentStatus(ApartmentStatus.UNAVAILABLE);
-        assertEquals(ApartmentStatus.UNAVAILABLE, apartment.getApartmentStatus());
-    }
-
-    @Test
-    void setApartmentType() {
-        Apartment apartment = new Apartment(1, BigDecimal.valueOf(100), ApartmentType.SINGLE);
-        apartment.setApartmentType(ApartmentType.DOUBLE);
-        assertEquals(ApartmentType.DOUBLE, apartment.getApartmentType());
-    }
-
     @Test
     void ToString() {
         Apartment apartment = new Apartment(1, BigDecimal.valueOf(100), ApartmentType.SINGLE);
@@ -69,6 +23,7 @@ class ApartmentTest {
     void Equals() {
         Apartment apartment1 = new Apartment(1, BigDecimal.valueOf(100), ApartmentType.SINGLE);
         Apartment apartment2 = new Apartment(1, BigDecimal.valueOf(150), ApartmentType.DOUBLE);
+
         assertNotEquals(apartment1, apartment2);
     }
 
@@ -76,6 +31,7 @@ class ApartmentTest {
     void HashCode() {
         Apartment apartment1 = new Apartment(1, BigDecimal.valueOf(100), ApartmentType.SINGLE);
         Apartment apartment2 = new Apartment(1, BigDecimal.valueOf(150), ApartmentType.DOUBLE);
+
         assertEquals(apartment1.hashCode(), apartment2.hashCode());
     }
 }
