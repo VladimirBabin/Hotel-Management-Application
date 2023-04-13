80841e20a1aa7199341a645e4c6fc99c88d75695
Mariana Arseneva <tailorsailor15@gmail.com>
2023-04-13 16:53:58 +0300

    Serialization

--- src/main/java/com/andersen_intensive/hotel/Serializer.java
+++ src/main/java/com/andersen_intensive/hotel/Serializer.java
@@ -6,6 +6,8 @@ import com.andersen_intensive.hotel.models.Reservation;
 import com.andersen_intensive.hotel.models.Utility;
 import lombok.SneakyThrows;
 import org.codehaus.jackson.map.ObjectMapper;
+import org.codehaus.jackson.node.ArrayNode;
+import org.codehaus.jackson.node.ObjectNode;
 
 import java.io.*;
 import java.util.List;
@@ -13,20 +15,33 @@ import java.util.List;
 public class Serializer {
 
     @SneakyThrows
-    public void serialization (List<Client> clients, List<Apartment> apartments,
-                               List<Reservation> reservations, List<Utility> utilities) throws RuntimeException {
-
+    public void serialization(List<Client> clients, List<Apartment> apartments,
+                              List<Reservation> reservations, List<Utility> utilities) throws RuntimeException {
         ObjectMapper objectMapper = new ObjectMapper();
-        String jsonClients = objectMapper.writeValueAsString(clients);
-        String jsonApartments = objectMapper.writeValueAsString(apartments);
-        String jsonReservations = objectMapper.writeValueAsString(reservations);
-        String jsonUtilities = objectMapper.writeValueAsString(utilities);
+        ObjectNode rootNode = objectMapper.createObjectNode();
+
+        if (!clients.isEmpty()) {
+            ArrayNode clientsNode = objectMapper.valueToTree(clients);
+            rootNode.put("clients", clientsNode);
+        }
+
+        if (!apartments.isEmpty()) {
+            ArrayNode apartmentsNode = objectMapper.valueToTree(apartments);
+            rootNode.put("apartments", apartmentsNode);
+        }
+
+        if (!reservations.isEmpty()) {
+            ArrayNode reservationsNode = objectMapper.valueToTree(reservations);
+            rootNode.put("reservations", reservationsNode);
+        }
+
+        if (!utilities.isEmpty()) {
+            ArrayNode utilitiesNode = objectMapper.valueToTree(utilities);
+            rootNode.put("utilities", utilitiesNode);
+        }
 
         try (FileWriter fileWriter = new FileWriter("D:\\hotelAfterComments\\ser")) {
-            fileWriter.write(jsonClients);
-            fileWriter.write(jsonApartments);
-            fileWriter.write(jsonReservations);
-            fileWriter.write(jsonUtilities);
+            objectMapper.writerWithDefaultPrettyPrinter().writeValue(fileWriter, rootNode);
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
