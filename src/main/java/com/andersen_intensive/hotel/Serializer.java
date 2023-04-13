206b9dcf1748924e0b7ae90da096991f79c3a943
Mariana Arseneva <tailorsailor15@gmail.com>
2023-04-13 17:06:06 +0300

    Serialization

--- src/main/java/com/andersen_intensive/hotel/Serializer.java
+++ src/main/java/com/andersen_intensive/hotel/Serializer.java
@@ -5,11 +5,14 @@ import com.andersen_intensive.hotel.models.Client;
 import com.andersen_intensive.hotel.models.Reservation;
 import com.andersen_intensive.hotel.models.Utility;
 import lombok.SneakyThrows;
+import org.codehaus.jackson.JsonNode;
 import org.codehaus.jackson.map.ObjectMapper;
 import org.codehaus.jackson.node.ArrayNode;
 import org.codehaus.jackson.node.ObjectNode;
+import org.codehaus.jackson.type.TypeReference;
 
 import java.io.*;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Serializer {
@@ -46,4 +49,41 @@ public class Serializer {
             throw new RuntimeException(e);
         }
     }
+
+    public void deserialization() throws RuntimeException {
+        try {
+            ObjectMapper objectMapper = new ObjectMapper();
+            File file = new File("D:\\hotelAfterComments\\ser");
+            JsonNode rootNode = objectMapper.readTree(file);
+
+            List<Client> clients = new ArrayList<>();
+            if (rootNode.has("clients")) {
+                JsonNode clientsNode = rootNode.get("clients");
+                clients = objectMapper.readValue(clientsNode.traverse(), new TypeReference<List<Client>>() {});
+            }
+
+            List<Apartment> apartments = new ArrayList<>();
+            if (rootNode.has("apartments")) {
+                JsonNode apartmentsNode = rootNode.get("apartments");
+                apartments = objectMapper.readValue(apartmentsNode.traverse(), new TypeReference<List<Apartment>>() {});
+            }
+
+            List<Reservation> reservations = new ArrayList<>();
+            if (rootNode.has("reservations")) {
+                JsonNode reservationsNode = rootNode.get("reservations");
+                reservations = objectMapper.readValue(reservationsNode.traverse(), new TypeReference<List<Reservation>>() {});
+            }
+
+            List<Utility> utilities = new ArrayList<>();
+            if (rootNode.has("utilities")) {
+                JsonNode utilitiesNode = rootNode.get("utilities");
+                utilities = objectMapper.readValue(utilitiesNode.traverse(), new TypeReference<List<Utility>>() {});
+            }
+
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
 }
